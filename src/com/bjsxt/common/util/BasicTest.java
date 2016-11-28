7eb8193becb46a10515050ea538b296c64a5400a
lellfdnn1 <hunbu200798@126.com>
2016-11-28 12:41:01 +0800

    sda

---
+++ src/com/bjsxt/common/util/BasicTest.java
@@ -0,0 +1,13 @@
+package com.bjsxt.common.util;
+
+
+public class BasicTest
+{
+	public static void main(String[] args)
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
